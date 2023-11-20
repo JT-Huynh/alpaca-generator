143bfb1eae43c6e94c3c855231813f50c94e69dc
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-11-20 22:36:12 +0700

    feat: added button for preview panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
+import { getAlpacaComponent } from "~/utils/fileHandler";
+
+function Button({ btnTxt }: { btnTxt: string }) {
+  return (
+    <button
+      type="button"
+      className="h-11 w-28 rounded-3xl border border-slate-400 p-2.5 tracking-widest text-slate-400"
+    >
+      {btnTxt}
+    </button>
+  );
+}
 
 export default function Home() {
+  getAlpacaComponent();
   return (
     <>
       <Head>
@@ -9,19 +22,20 @@ export default function Home() {
         <meta name="description" content="Generate alpaca image" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-extrabold uppercase tracking-wider text-blue-950">
+      <section className="flex h-screen w-screen flex-col justify-start gap-10 bg-zinc-100 px-48 py-20">
+        <h1 className="font-sans text-5xl font-bold uppercase tracking-widest text-slate-900">
           alpaca generator
         </h1>
-        <div>
+        <div className="flex gap-48">
           <Image
-            src="/assets/alpaca/backgrounds/blue50.png"
+            src="/assets/alpaca/backgrounds/darkblue50.png"
             width={500}
             height={500}
             alt="preview image"
           />
+          <Button btnTxt="Hair" />
         </div>
-      </main>
+      </section>
     </>
   );
 }
