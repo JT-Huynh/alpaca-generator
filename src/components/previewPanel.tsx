226619443518140d999e92cacdc37a356f03b5cb
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-11-27 09:06:44 +0700

    feat: preview image 1

--- src/components/previewPanel.tsx
+++ src/components/previewPanel.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 
 export default function PreviewPanel() {
   return (
-    <div>
+    <div className="relative h-96 w-96">
       <Image
-        className="min-h-fit rounded"
+        className="absolute left-0 top-0 h-96 w-96"
         src="/alpaca/backgrounds/darkblue50.png"
+        width={570}
+        height={570}
+        alt="preview image"
+      />
+      <Image
+        className="absolute"
+        src="/alpaca/neck/default.png"
         width={570}
         height={570}
         alt="preview image"
