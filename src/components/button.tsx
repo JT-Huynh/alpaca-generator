262e202f9805da388c4d5fc825166647b6d1825c
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-12-27 22:03:23 +0700

    refactor: classname

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,15 +9,16 @@ export default function Button({
   isActive?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }) {
+  const inActiveClasses =
+    "text-blue-400 hover:border-blue-600 hover:text-blue-600 active:bg-blue-600 active:text-zinc-100";
+  const activeClasses = "bg-blue-600 text-zinc-100";
   return (
     <button
       value={value}
       type="button"
       onClick={onClick}
       className={`min-w-fit rounded-3xl border border-blue-400 px-8 py-2.5 text-center text-sm font-bold capitalize tracking-widest shadow transition-all focus:shadow-blue-600 focus:outline-none ${
-        isActive
-          ? "bg-blue-600 text-zinc-100"
-          : "text-blue-400 hover:border-blue-600 hover:text-blue-600 active:bg-blue-600 active:text-zinc-100 "
+        isActive ? activeClasses : inActiveClasses
       }`}
     >
       {name}
